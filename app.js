e8231bf5dc56f78f08e0b86b054b1d846717fd0c
Code with Susant <154358595+Susanta07@users.noreply.github.com>
2024-10-20 14:45:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ function takeCommand(message){
     }
 
     else {
-        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        window.open(`https://m.youtube.com/#searching${message.replace(" ", "+")}`, "_blank");
         const finalText = "I found some information for " + message + " on google";
         speak(finalText);
     }
